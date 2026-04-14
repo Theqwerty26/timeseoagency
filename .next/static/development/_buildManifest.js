c7d781adad1048ad97c2b59b8ae5f9ab99d4bcbe
Theqwerty26 <121343712+Theqwerty26@users.noreply.github.com>
2026-04-15 00:27:52 +0300

    real cleanup

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/hakkimizda":["static\u002Fchunks\u002Fpages\u002Fhakkimizda.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fhakkimizda"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
